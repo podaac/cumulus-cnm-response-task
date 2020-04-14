428443995c13bc7889f6d57167fd357e95443b62
nchung <nga.t.chung@jpl.nasa.gov>
2020-04-14 16:27:20 -0700

    Added license.

--- artifact.sh
+++
@@ -1,3 +0,0 @@
-mvn dependency:copy-dependencies
-gradle -x test build
-curl -u jenkins:${JENKINS_ARTIFACT_TOKEN} -X PUT https://podaac-ci.jpl.nasa.gov:8443/artifactory/ext-release-local/gov/nasa/cumulus/cnmResponse/1.0.5/cnmResponse-1.0.5.zip  -T build/distributions/cnmResponse-1.0.5.zip
